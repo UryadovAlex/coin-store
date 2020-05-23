670f74e522a60bfaa9a96d0b68772536503f010a
Aleksandr Uryadov <uryadov212@gmail.com>
2020-05-24 01:07:55 +0400

    create.sql changed

--- create.sql
+++ create.sql
@@ -52,8 +52,7 @@ CREATE TABLE IF NOT EXISTS `bootcamp`.`cart` (
     ON DELETE CASCADE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 39
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
